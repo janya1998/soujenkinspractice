37dab9b1702aaf05469cb011c36749e0428440b0
janya1998 <soujanyas2712@gmail.com>
2022-05-03 09:43:25 +0530

    this my commit

---
+++ src/test/java/CreateContact/CreateOrg.java
@@ -0,0 +1,21 @@
+package CreateContact;
+
+import org.testng.annotations.Test;
+
+public class CreateOrg {
+	@Test
+
+	public void CreateOrg() {
+		
+		System.out.println("createorg");
+	}
+
+
+	@Test
+	public void createOrg() {
+		System.out.println("createorg1");
+	}
+		
+	
+
+}
